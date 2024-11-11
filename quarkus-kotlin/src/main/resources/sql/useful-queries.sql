d03bb5555a7c64aaaca5d74d1eec88877202e58c
Sid-Athos <sa.bennaceur@gmail.com>
2024-11-11 04:03:11 +0100

    test tags

--- quarkus-kotlin/src/main/resources/sql/useful-queries.sql
+++ quarkus-kotlin/src/main/resources/sql/useful-queries.sql
@@ -6,3 +6,8 @@ WHERE ptp.pretnder_id = (SELECT p.id FROM pretenders p WHERE p.reference = '8a87
 --- Update a trait pair score
 
 UPDATE pretender_trait_pairs set score = 100 WHERE reference = 'b14fc0f0a7df4cf2942ccdb96e5538b5'
+-- find picture order for insert purpose
+SELECT count(*) FROM profile_pics pp WHERE pp.pretenders_id = (SELECT p.id FROM pretenders p WHERE p.reference =
+                                                                                                   '8a870bf8d386475d88e91f98720f5efe');
+-- update picture order
+UPDATE profile_pics SET pic_order = 5 WHERE reference = '123456789';
